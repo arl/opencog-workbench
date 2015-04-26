6294f74ece549a2886f87bc67c734eb47dc0dfe6
aurelien-rainone <aurelien.rainone@gmail.com>
2015-04-26 23:27:36 +0200

    clean dummymodule

--- src/client/app/dummymodule/dummymodule.js
+++ src/client/app/dummymodule/dummymodule.js
@@ -6,7 +6,7 @@
         .controller('Dummymodule', Dummymodule);
 
     /* @ngInject  */
-    function Dummymodule($scope, $rootScope, $q, dataservice, logger, dummymoduleConstants, menuhelper, dummymenus) {
+    function Dummymodule(logger, dummymoduleConstants, dummymenus) {
 
         /*jshint validthis: true */
         var vm = this;
@@ -17,39 +17,10 @@
         };
         vm.title = dummymoduleConstants.name;
 
+        // define menu click handler
         dummymenus.onCallDummy(function() {
 
             console.log('dummymenus.onClickDummy');
-            var called = true;
-        });
-
-
-        var menuListener = $rootScope.$on('clickMenu', function (event, menuid) {
-        if (menuid === dummymoduleConstants.id + 'item1.1') {
-            alert('dummymodule: onClickMe, title : ' + vm.title);
-        }
-/*
-        il faut deporter vers menuhelper tout le code generique, qui serait répeté
-        a chaque controller principal des modules.
-        Mais, sachant des des controllers differents d'un meme module peuvent consomer un
-        menu, il faut trouver un moyen de passer le controler avec le menu dont on souhaite etre notifie
-        de plusil ya le probleme des menus 'checkboxes', peut etre passer a une certaine fonction de 
-        menuhelper la variable a updater automatiquement en cas de changement de valeur de la checkbox
-
-        // something like that 
-        menuhelper.registerHandler(moduleConstant.id, 'item1.1', )
-
-        menuhelper.onItem1( function () {
-            
-        });
-
-        on.destroy(menuhelper.unregisterHandlers())
-*/
-        });
-
-        $scope.$on('$destroy', function() {
-            var coucou = true;
-            menuListener();
         });
 
         activate();
