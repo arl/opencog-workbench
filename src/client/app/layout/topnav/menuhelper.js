636fcc29730b9ab490b8ab0961741e57d4b3eee3
aurelien-rainone <aurelien.rainone@gmail.com>
2015-05-01 11:58:31 +0200

    rename topnavmenu in topnav and add related files

--- src/client/app/layout/topnav/menuhelper.js
+++ src/client/app/layout/topnav/menuhelper.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app.layout.topnavmenu')
+        .module('app.layout.topnav')
         .factory('menuhelper', menuhelper)
         .directive('checkboxMenuitem', checkboxMenuitem);
 
